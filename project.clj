c71d57708964a059eb432d7d589f67bbdb4bfd33
Assen Kolov <assen.kolov@gmail.com>
2015-03-28 17:01:59 +0100

    added repositories

--- project.clj
+++ project.clj
@@ -9,10 +9,13 @@
                            ]
 
             :profiles {
-                        :dev {:dependencies [[midje "1.6.3"]
-                                             [midje-junit-formatter "0.1.0-SNAPSHOT"]]
+                       :dev {:dependencies [[midje "1.6.3"]
+                                            [midje-junit-formatter "0.1.0-SNAPSHOT"]]
 
-                              :plugins      [[lein-deps-tree "0.1.2"]
-                                             [lein-midje "3.0.0"]
-                                             [test2junit "1.0.1"]]}}
+                             :plugins      [[lein-deps-tree "0.1.2"]
+                                            [lein-midje "3.0.0"]
+                                            [test2junit "1.0.1"]]}}
+            :repositories [
+                           ["snapshots" "http://nexus.akolov.com/content/repositories/snapshots"]
+                           ["releases" "http://nexus.akolov.com/content/repositories/releases"]]
             )
